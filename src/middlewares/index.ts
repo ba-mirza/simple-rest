ca0915d02b856ec89826327fca28712c433c3406
Almas <almas1610@gmail.com>
2023-08-16 19:22:37 +0600

    init simple rest-api

---
+++ src/middlewares/index.ts
@@ -0,0 +1,54 @@
+import express from "express";
+import { get, merge } from "lodash";
+
+import { getUserBySessionToken } from "db/users";
+import { TSVOPER_AUTH } from "controllers/authentication";
+
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "identity._id") as string;
+
+    if (!currentUserId) {
+      return res.sendStatus(403);
+    }
+
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
+};
+
+export const isAuthenticated = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const sessionToken = req.cookies[TSVOPER_AUTH];
+
+    if (!sessionToken) {
+      return res.sendStatus(403);
+    }
+
+    const existingUser = await getUserBySessionToken(sessionToken);
+
+    if (!existingUser) {
+      return res.sendStatus(403);
+    }
+
+    merge(req, { identity: existingUser });
+
+    return next();
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
+};
